fb1890eada545ccb5b61dbfa3bcf342ad22c65e4
akshatsabavat <akshat.sabavat@gmail.com>
2024-03-22 01:38:25 +0530

    Chore: Removed boilerplate + added basic routes

---
+++ src/pages/events/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+const CampusEvents = () => {
+  return <Text>Campus Events</Text>;
+};
+
+export default CampusEvents;
---
+++ src/pages/tour/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+const CampusWalkthrough = () => {
+  return <Text>Walkthrough here page</Text>;
+};
+
+export default CampusWalkthrough;
