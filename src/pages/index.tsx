f6caa68d7eb5fcdf474791dfec47cba80b1f54e4
akshatsabavat <akshat.sabavat@gmail.com>
2024-03-22 12:40:38 +0530

    Chore: Removed more boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
 import { api } from "~/utils/api";
 
 export default function Home() {
@@ -7,6 +7,10 @@ export default function Home() {
   return (
     <Flex flexDir="column">
       <Text>Index Page</Text>
+      <Flex gap={2}>
+        <Button as="a" href="/tour">Campus Tour</Button>
+        <Button as="a" href="/events">Campus Events</Button>
+      </Flex>
       <Text>{JSON.stringify(hello.data?.greeting)}</Text>
     </Flex>
   );
