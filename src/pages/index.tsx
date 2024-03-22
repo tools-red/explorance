9dc925ae819810e73676933451f56225d7ea0109
akshatsabavat <akshat.sabavat@gmail.com>
2024-03-22 18:51:59 +0530

    Chore: removed hook directory

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
 import { api } from "~/utils/api";
+import useServerActions from "./_hooks/useServerActions";
+import { useState } from "react";
 
 export default function Home() {
+  const [showLoader, setShowLoader] = useState<boolean>(false);
+
   const hello = api.user.hello.useQuery({ text: "from server" });
+
   return (
     <Flex flexDir="column">
       <Text>Index Page</Text>
@@ -13,7 +18,15 @@ export default function Home() {
         <Button as="a" href="/events">
           Campus Events
         </Button>
-        <Button>Fetch Bucket Items</Button>
+        <Button
+          onClick={async () => {
+            setShowLoader(true);
+            const bucketItems = api.r2.fetchBucketContent.useQuery();
+            console.log(bucketItems);
+          }}
+        >
+          Fetch Bucket Items
+        </Button>
       </Flex>
       <Text>{JSON.stringify(hello.data?.greeting)}</Text>
     </Flex>
