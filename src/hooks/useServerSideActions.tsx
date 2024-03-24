688edddf4fff2c9db08efac157e84813cbfa090d
akshatsabavat <akshat.sabavat@gmail.com>
2024-03-24 11:00:14 +0530

    Added: Hook for server side actions

---
+++ src/hooks/useServerSideActions.tsx
@@ -0,0 +1,24 @@
+import { TRPCClientErrorLike } from "@trpc/client";
+import { useState } from "react";
+import { api } from "~/utils/api";
+
+const useServerSideActions = () => {
+  const [error, setError] = useState<TRPCClientErrorLike<{
+    input: void;
+    output: any[];
+    transformer: boolean;
+    errorShape: any;
+  }> | null>(null);
+
+  const {
+    data,
+    isLoading,
+    refetch: fetchBucketContent,
+  } = api.r2.fetchBucketContent.useQuery(undefined, {
+    enabled: false,
+  });
+
+  return { error, fetchBucketContent, data, isLoading };
+};
+
+export default useServerSideActions;
