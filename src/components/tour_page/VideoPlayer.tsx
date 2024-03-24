a7368dc5ea8e8024bd96de6a8e2d07218bed7626
akshatsabavat <akshat.sabavat@gmail.com>
2024-03-24 13:59:38 +0530

    Created: Video Player component files

---
+++ src/components/tour_page/VideoPlayer.tsx
@@ -0,0 +1,7 @@
+export const getServerSideProps: GetServerSideProps<{}> = async ({
+  params,
+}) => {};
+
+const VideoPlayer = () => {};
+
+export default VideoPlayer;
--- src/pages/tour/index.tsx
+++ src/pages/tour/index.tsx
@@ -1,4 +1,5 @@
 import { Text } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
 
 const CampusWalkthrough = () => {
   return <Text>Walkthrough here page</Text>;
