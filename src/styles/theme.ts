c4a9925f309c10cdce48ec28316f640e4da20fc6
akshatsabavat <akshat.sabavat@gmail.com>
2024-03-25 15:50:42 +0530

    Created: Theme object

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    designSystem: {
+      containerBackground: "",
+    },
+  },
+});
+
+export default theme;
