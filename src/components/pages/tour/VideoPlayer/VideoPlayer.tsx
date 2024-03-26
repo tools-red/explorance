e0503c36119063b21c8b238d1aa7d8cdedf4b2e2
Somesh Kar <somesh.kar@gmail.com>
2024-03-26 09:50:18 +0530

    fix(tour page): make video player fit container dimensions

--- src/components/pages/tour/VideoPlayer/VideoPlayer.tsx
+++ src/components/pages/tour/VideoPlayer/VideoPlayer.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Box } from "@chakra-ui/react";
 import ReactPlayer from "react-player";
 
@@ -6,22 +7,34 @@ interface VideoPlayerProps {
 }
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoFile }) => {
+    const [calcWidth, setCalcWidth] = useState<number | null>(null);
+
+  useEffect(() => {
+    const viewportHeight = window.innerHeight;
+    const aspectRatio = 16 / 9;
+    const newCalcWidth = viewportHeight * aspectRatio;
+    setCalcWidth(newCalcWidth);
+  }, []);
+
+
   return (
-    <Box width="100%" height="100%" position="relative" overflow="hidden">
-      <ReactPlayer
-        loop={true}
-        playing={true}
-        width="100%"
-        height="100%"
-        url={`${process.env.NEXT_PUBLIC_CLOUDFLARE_PUBLIC_URL}${videoFile}`}
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          objectFit: "cover",
-        }}
-      />
+    <Box width="100%" height="100%" position="relative" overflow="hidden" borderRadius="22px">
+      {calcWidth && (
+        <ReactPlayer
+          loop={true}
+          playing={true}
+          width={calcWidth}
+          height="100%"
+          url={`${process.env.NEXT_PUBLIC_CLOUDFLARE_PUBLIC_URL}${videoFile}`}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            objectFit: "cover",
+          }}
+        />
+      )}
     </Box>
   );
 };
