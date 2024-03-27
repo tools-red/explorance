7bbe1ee3bec886090ade811b79ef41bdc2e65fbb
akshatsabavat <akshat.sabavat@gmail.com>
2024-03-27 12:12:29 +0530

    Removed: Unnecessary Atom types

--- src/lib/atom.ts
+++ src/lib/atom.ts
@@ -1,6 +1,6 @@
 import { useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
-import { videoSelectedAtomState, videoSequenceAtomState } from "~/types";
+import { videoSequenceAtomState } from "~/types";
 
 export const VideoSequenceAtom = atomWithStorage<videoSequenceAtomState>(
   "videoSequenceAtomState",
@@ -9,12 +9,4 @@ export const VideoSequenceAtom = atomWithStorage<videoSequenceAtomState>(
   }
 );
 
-export const VideoSelectedAtom = atomWithStorage<videoSelectedAtomState>(
-  "videoSelectedAtomState",
-  {
-    video: null,
-  }
-);
-
 export const useVideoSequenceAtom = () => useAtom(VideoSequenceAtom);
-export const useVideoSelectedAtom = () => useAtom(VideoSelectedAtom);
--- src/types.ts
+++ src/types.ts
@@ -5,24 +5,9 @@ type WalkthroughData = {
   scriptContent: string;
 }[];
 
-// atom body types
-type videoSelectedAtomBody = {
-  sequenceNumber: string | undefined;
-  videoFile: string | undefined;
-  scriptContent: string | undefined;
-};
-
 // atom state types
 type videoSequenceAtomState = {
   sequence: number;
 };
 
-type videoSelectedAtomState = {
-  video: videoSelectedAtomBody | null;
-};
-
-export {
-  type WalkthroughData,
-  type videoSequenceAtomState,
-  type videoSelectedAtomState,
-};
+export { type WalkthroughData, type videoSequenceAtomState };
