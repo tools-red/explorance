e506cd7e2eb0c06eea86566177bc45576297f728
akshatsabavat <akshat.sabavat@gmail.com>
2024-03-28 12:25:29 +0530

    Added: function to fetch user audio device and initiate recording

--- src/hooks/useAudioActions.tsx
+++ src/hooks/useAudioActions.tsx
@@ -1,7 +1,31 @@
-const useAudioActions = () => {
-  const getAudio = () => {};
+import { useRef, useState } from "react";
 
-  return { getAudio };
+const useAudioActions = () => {
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const [isRecording, setIsRecording] = useState<boolean>();
+  const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
+
+  const initiateRecording = async () => {
+    try {
+      const audio_stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+      const mediaRecorder = new MediaRecorder(audio_stream);
+      mediaRecorderRef.current = mediaRecorder;
+      mediaRecorder.start();
+      setIsRecording(true);
+
+      mediaRecorder.ondataavailable = (event) => {
+        if (event.data && event.data.size > 0) {
+          setAudioChunks((prevChunks) => [...prevChunks, event.data]);
+        }
+      };
+    } catch (err) {
+      console.error("Error in accessing media devices: ", err);
+    }
+  };
+
+  return { initiateRecording };
 };
 
 export default useAudioActions;
