cf46a295b090577ba931084adff11fc83f0795e2
akshatsabavat <akshat.sabavat@gmail.com>
2024-03-30 22:14:02 +0530

    Chore: Cleaned cod4e

--- src/pages/tour/index.tsx
+++ src/pages/tour/index.tsx
@@ -1,25 +1,11 @@
-import {
-  Box,
-  Button,
-  Circle,
-  Flex,
-  Grid,
-  GridItem,
-  Icon,
-  Text,
-} from "@chakra-ui/react";
-import { IoSparklesSharp } from "react-icons/io5";
+import { Box } from "@chakra-ui/react";
 import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import React from "react";
-import EvitarContainer from "~/components/pages/tour/EvitarContainer";
-import VideoPlayerContainer from "~/components/pages/tour/VideoPlayer/VideoPlayerContainer";
-import VoiceChatContainer from "~/components/pages/tour/VoiceChat/VoiceChatContainer";
-import useServerSideActions from "~/hooks/useServerSideActions";
 import { useVideoSequenceAtom } from "~/lib/atom";
-import { MotionBox, MotionCircle } from "~/lib/framer";
 import { graphQL } from "~/lib/graphQL";
 import { WalkthroughData } from "~/types";
-import { shiningAnimation } from "~/lib/keyframes";
+
+import React from "react";
+import useServerSideActions from "~/hooks/useServerSideActions";
 import BeginExperiencePage from "~/components/pages/tour/PageComponents/BeginExperiencePage";
 import VideoExperiencePage from "~/components/pages/tour/PageComponents/VideoExperiencePage";
 
