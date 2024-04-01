0eb0a809d2fed24030448815339bf6d7c6efb241
akshatsabavat <akshat.sabavat@gmail.com>
2024-04-01 12:24:38 +0530

    Progress

--- src/components/pages/tour/VideoAvatar/EvitarContainer.tsx
+++ src/components/pages/tour/VideoAvatar/EvitarContainer.tsx
@@ -1,17 +1,39 @@
 import { Flex, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useAvatarActions from "~/hooks/useAvatarActions";
+import IntroductionPlayer from "./IntroductionPlayer";
 
 export const EvitarContainer = () => {
+  const { aiVideos } = useAvatarActions();
+  useEffect(() => {
+    console.log(aiVideos);
+  }, [aiVideos]);
+
+  const [introEnd, setIntroEnd] = useState<boolean>(false);
+
+  const videosFiltered = aiVideos?.filter((video) =>
+    video?.Key?.includes("library")
+  );
+
+  console.log(videosFiltered);
+
   return (
     <Flex
       borderRadius={22}
       backgroundColor="#1E1E1E"
-      flexDir="column"
-      justify="center"
-      align="center"
-      // height={400}
-      // width={250}
+      justifyContent="center"
+      alignItems="center"
+      width="full" // Make the container full width
+      height="full" // Make the container full height
+      position="relative" // Ensure positioning context for the absolute-positioned child
+      overflow="hidden" // Hide overflow to prevent scrollbars
     >
-      <Text color="white">Evitar Container</Text>
+      {videosFiltered.length > 0 && (
+        <IntroductionPlayer
+          setIntroEnd={setIntroEnd}
+          videoFile={aiVideos[1]?.Key as string}
+        />
+      )}
     </Flex>
   );
 };
--- src/hooks/useAvatarActions.tsx
+++ src/hooks/useAvatarActions.tsx
@@ -1,3 +1,42 @@
-const useAvatarActions = () => {};
+import { useEffect, useState } from "react";
+import { api } from "~/utils/api";
+
+const useAvatarActions = () => {
+  const [aiVideos, setAiVideos] = useState<
+    { ETag: string | undefined; Key: string | undefined }[]
+  >([]); // Corrected state initialization
+
+  const { refetch: fetchBucketContent } = api.r2.fetchBucketContent.useQuery(
+    undefined,
+    {
+      enabled: false,
+    }
+  );
+
+  const loadAIExperience = async () => {
+    try {
+      const response = await fetchBucketContent();
+      const videos = response.data?.CDN_Response;
+      const aiVideos = videos?.filter((video) => video?.Key?.includes("AI"));
+      const filteredAiVideoData =
+        aiVideos?.map((item) => ({
+          ETag: item.ETag,
+          Key: item.Key,
+        })) || [];
+
+      // Set the filtered AI videos to the state
+      setAiVideos([...filteredAiVideoData]);
+    } catch (error) {
+      // Handle errors here
+      console.error("Error loading AI experience:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadAIExperience();
+  }, []);
+
+  return { aiVideos }; // Export aiVideos state
+};
 
 export default useAvatarActions;
