e7822d188ffffd692e553f782c06187a211ace4e
akshatsabavat <akshat.sabavat@gmail.com>
2024-04-26 10:17:23 +0530

    Fixed: Chat UI

--- src/components/pages/events/EventSlugSpecific/Chat.tsx
+++ src/components/pages/events/EventSlugSpecific/Chat.tsx
@@ -6,11 +6,13 @@ import {
   Input,
   InputGroup,
   InputRightAddon,
+  InputRightElement,
   Text,
 } from "@chakra-ui/react";
 
 import { IoMicOutline } from "react-icons/io5";
 import { IoChatboxEllipses } from "react-icons/io5";
+import { IoIosSend } from "react-icons/io";
 
 interface ChatProps {
   setInput: (input_value: string) => void;
@@ -33,7 +35,7 @@ const Chat: React.FC<ChatProps> = ({
 
   return (
     <Box h={300} borderRadius={20} bg="#181818">
-      <Flex pb={3} gap={3} h="full" justify="flex-end" flexDir="column">
+      <Flex pb={3.5} gap={3} h="full" justify="flex-end" flexDir="column">
         <Flex justify="center" align="center" h="full">
           <Flex gap={2} align="center" flexDir="column">
             <Icon
@@ -55,8 +57,9 @@ const Chat: React.FC<ChatProps> = ({
             </Text>
           </Flex>
         </Flex>
-        <InputGroup>
-          <InputRightAddon border="none" bg="none">
+        <Flex justify="center">
+          <InputGroup px={3} w={"100%"}>
+            {/* <InputRightAddon border="none" bg="none">
             <Icon
               color="white"
               bg="#1B1B1B"
@@ -67,28 +70,63 @@ const Chat: React.FC<ChatProps> = ({
               boxSize={10}
               as={IoMicOutline}
             />
-          </InputRightAddon>
-          <Input
-            borderRadius={12}
-            color="white"
-            bg="#1B1B1B"
-            border="1px"
-            borderColor="rgba(255, 255, 255, 0.1)"
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Ask AI anything"
-            fontSize="small"
-            onKeyDown={handleKeyDown}
-            _hover={{
-              borderColor: "purple.500",
-            }}
-            _placeholder={{
-              color: "rgba(255, 255, 255, 0.6)",
-            }}
-          />
-        </InputGroup>
-        {/* <Button onClick={() => handleSendQueryToAi(user_input, transcription_id)}>
-        Send
-      </Button> */}
+          </InputRightAddon> */}
+            <Input
+              borderRadius={10}
+              color="white"
+              bg="#1B1B1B"
+              border="1px"
+              borderColor="rgba(255, 255, 255, 0.1)"
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Ask AI anything"
+              fontSize="small"
+              onKeyDown={handleKeyDown}
+              _hover={{
+                borderColor: "purple.500",
+              }}
+              _placeholder={{
+                color: "rgba(255, 255, 255, 0.6)",
+              }}
+            />
+            <InputRightElement mr={7}>
+              <Flex gap={1}>
+                <Icon
+                  onClick={() => console.log("Mic")}
+                  _hover={{
+                    cursor: "pointer",
+                    bg: "white",
+                    color: "#1B1B1B",
+                  }}
+                  color="white"
+                  bg="#1B1B1B"
+                  border="1px"
+                  borderColor="rgba(255, 255, 255, 0.1)"
+                  borderRadius="20%"
+                  p={0.5}
+                  boxSize={7}
+                  as={IoMicOutline}
+                  transition="all 0.2s"
+                />
+                <Icon
+                  _hover={{
+                    cursor: "pointer",
+                    bg: "white",
+                    color: "#1B1B1B",
+                  }}
+                  color="white"
+                  bg="#1B1B1B"
+                  border="1px"
+                  borderColor="rgba(255, 255, 255, 0.1)"
+                  borderRadius="20%"
+                  p={0.5}
+                  boxSize={7}
+                  as={IoIosSend}
+                  transition="all 0.2s"
+                />
+              </Flex>
+            </InputRightElement>
+          </InputGroup>
+        </Flex>
       </Flex>
     </Box>
   );
