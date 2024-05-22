50b273c6e5eb01a0cadba43dd20add98a24da895
akshatsabavat <akshat.sabavat@gmail.com>
2024-05-22 10:30:08 +0530

    Created: File for constants, Navbar constants added

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+// Landing page constants
+
+export const URLS: { url_label: string; url_location: string }[] = [
+  { url_label: "About", url_location: "/" },
+  { url_label: "Integrations", url_location: "/" },
+  { url_label: "Pricing", url_location: "/" },
+  { url_label: "Features", url_location: "/" },
+  { url_label: "Contact", url_location: "/" },
+];
