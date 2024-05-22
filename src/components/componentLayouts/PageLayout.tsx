d6cf84ca0bd509eb3dc6515a6a7e463509b6b9a7
akshatsabavat <akshat.sabavat@gmail.com>
2024-05-22 11:44:20 +0530

    Added: Styling to navbar

--- src/components/componentLayouts/PageLayout.tsx
+++ src/components/componentLayouts/PageLayout.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import React from "react";
 
 interface PageLayoutProps {
--- src/components/global/Navbar/index.tsx
+++ src/components/global/Navbar/index.tsx
@@ -6,14 +6,35 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ URLs }) => {
   return (
-    <Flex>
-      {URLs.map((navItem) => {
-        return (
-          <Text as="a" href={navItem.url_location}>
-            {navItem.url_label}
-          </Text>
-        );
-      })}
+    <Flex align="center" mt={5} justify="space-between">
+      <Text color="white">Explorance</Text>
+      <Flex
+        gap={8}
+        px={5}
+        py={2}
+        border="1px solid"
+        borderColor="gray.700"
+        color="white"
+        borderRadius={20}
+      >
+        {URLs.map((navItem, index) => {
+          return (
+            <Flex key={index} gap={6} align="center" bg="none">
+              <Text fontSize="small" as="a" href={navItem.url_location}>
+                {navItem.url_label}
+              </Text>
+              {index < URLs.length - 1 ? (
+                <Text color="#A0AEE5" bg="none">
+                  &#x2022;
+                </Text>
+              ) : (
+                <></>
+              )}
+            </Flex>
+          );
+        })}
+      </Flex>
+      <Text>Get Started</Text>
     </Flex>
   );
 };
