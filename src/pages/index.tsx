ff33578a36ab2f51513ed47a6e39075d2dcf8cb3
akshatsabavat <akshat.sabavat@gmail.com>
2024-05-22 10:31:57 +0530

    Chore: cleaned index page text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,5 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
-import { useEffect } from "react";
-import useServerSideActions from "~/hooks/useServerSideActions";
 
 export default function Home() {
-  const { filterWalkthroughData } = useServerSideActions();
-  useEffect(() => {
-    const asyncF = async () => {
-      await filterWalkthroughData();
-    };
-
-    asyncF().catch((err) => {
-      console.log(`error --> : ${err}`);
-    });
-  }, []);
-  return (
-    <Flex flexDir="column">
-      <Text>Index Page</Text>
-      <Flex gap={2}>
-        <Button as="a" href="/tour">
-          Campus Tour
-        </Button>
-        <Button as="a" href="/events">
-          Campus Events
-        </Button>
-      </Flex>
-    </Flex>
-  );
+  return <Flex flexDir="column"></Flex>;
 }
