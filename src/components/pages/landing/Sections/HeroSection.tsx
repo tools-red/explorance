3a1eb2790681af46ff31f41139d31107efbe1b83
akshatsabavat <akshat.sabavat@gmail.com>
2024-05-29 23:29:40 +0530

    Completed: Landing page

--- src/components/pages/landing/Sections/HeroSection.tsx
+++ src/components/pages/landing/Sections/HeroSection.tsx
@@ -1,18 +1,21 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { URLs } from "~/constants";
 
 import { FaCompass } from "react-icons/fa6";
 import { FaMicrophone } from "react-icons/fa";
 
+import HeroImage from "../../../../../public/StaticImages/HeroImg.png";
+
 import Navbar from "../../../global/Navbar";
 import Tag from "~/components/global/Text/Tag";
 import PrimaryLinkCTA from "~/components/global/Buttons/PrimaryLinkCTA";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <Flex flexDir="column">
       <Navbar URLs={URLs} />
-      <Flex mt={100} gap={4} align="center" w="full" flexDir="column">
+      <Flex mt="80px" gap={4} align="center" w="full" flexDir="column">
         <Tag tag_label="Experience more in less time with AI" />
         <Text
           bgGradient="linear(to-b, #A1AEE5 0%, #ECF0FF 100%)"
@@ -51,9 +54,19 @@ const HeroSection = () => {
             CTA_labelSize="small"
             CTA_addOn={false}
             CTA_label="Explore Campus Events"
-            CTA_location="/tour"
+            CTA_location="/events"
           />
         </Flex>
+        <Box
+          border="1px solid"
+          borderColor="rgba(161, 174, 229, 0.2)"
+          borderRadius={20}
+          overflow={"hidden"}
+          mt={8}
+          width={900}
+        >
+          <Image alt="Hero-Image" src={HeroImage} objectFit="cover" />
+        </Box>
       </Flex>
     </Flex>
   );
