8c061a61c90f73f10876067ebc8d218248a08fd9
akshatsabavat <akshat.sabavat@gmail.com>
2024-06-06 01:30:59 +0530

    Defined: Secondary Card Type component

---
+++ src/components/pages/landing/Cards/SecondaryCardType.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+
+interface SecondaryCardTypeProps {}
+
+const SecondaryCardType: React.FC<SecondaryCardTypeProps> = () => {
+  return <Flex flexDir="column"></Flex>;
+};
+
+export default SecondaryCardType;
