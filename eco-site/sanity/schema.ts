8babb01272bc426fcf7d03d9f6f8b871a80c8442
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-13 12:18:47 +0530

    Created: Test product schema

--- eco-site/sanity/schema.ts
+++ eco-site/sanity/schema.ts
@@ -1,5 +1,34 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition, StringSchemaType } from "sanity";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [
+    {
+      name: "product",
+      title: "Product",
+      type: "document",
+      fields: [
+        {
+          name: "productTitle",
+          title: "Product Title",
+          type: "string",
+        },
+        {
+          name: "productPrice",
+          title: "Product Price",
+          type: "number",
+        },
+        {
+          name: "slug",
+          title: "Slug",
+          type: "slug",
+          options: { source: "name" },
+        },
+        {
+          name: "productDescription",
+          title: "Product Description",
+          type: "string",
+        },
+      ],
+    },
+  ],
+};
