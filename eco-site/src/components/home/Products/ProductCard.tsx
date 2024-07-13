deb84201470d802bb7c83850cb48568cdbf785b5
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-13 16:48:40 +0530

    Added: Temporary Card Layouts

--- eco-site/src/components/home/Products/ProductCard.tsx
+++ eco-site/src/components/home/Products/ProductCard.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Icon, Text } from "@chakra-ui/react";
 import { ContentLakeProductsType } from "~/types/contentLake";
+import { FaCartPlus } from "react-icons/fa";
 
 interface ProductCardProps {
   product: ContentLakeProductsType;
@@ -7,13 +8,22 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
-    <Flex flexDir="column">
-      <Box h={200} w={150} bgColor="purple.500" />
+    <Flex w={180} flexDir="column">
+      <Box h={200} w="full" bgColor="purple.500" />
       <Flex mt={2} flexDir="column">
         <Text fontWeight={600}>{product.productTitle}</Text>
-        <Flex>
-          <Text>{product.productPrice}</Text>
-          <Button>Open</Button>
+        <Flex align="center" justify="space-between">
+          <Text>${product.productPrice}</Text>
+          <Button
+            variant="none"
+            bg="green.200"
+            size="small"
+            p={2}
+            fontSize="small"
+            rightIcon={<Icon as={FaCartPlus} />}
+          >
+            Add to Cart
+          </Button>
         </Flex>
       </Flex>
     </Flex>
