315fb999486a4b58efb96d8f1eebff07d8772887
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-13 13:59:57 +0530

    Changed: Fetch content lake products data function

--- eco-site/src/hooks/useSanityContentLake.tsx
+++ eco-site/src/hooks/useSanityContentLake.tsx
@@ -1,16 +1,31 @@
+import { FetchProductsFromSanityContentLakeType } from "~/types/promises";
 import { client } from "../../sanity/lib/client";
 import { fetchAllProducts } from "../../sanity/lib/queries";
+import { Dispatch, SetStateAction } from "react";
+import { ContentLakeProductsType } from "~/types/contentLake";
 
-const useSanityContentLake = () => {
-  const fetchProductsFromContentLake = async () => {
-    try {
-      const productsData = await client.fetch(fetchAllProducts);
-      return;
-    } catch (err) {
-      console.log("Error");
-      throw new Error("Error while fetching products");
-    }
-  };
+const useSanityContentLake = (
+  setIsFetching: Dispatch<SetStateAction<boolean>>,
+  setContentLakeProducts: Dispatch<SetStateAction<ContentLakeProductsType>>,
+  setErrorMessage: Dispatch<SetStateAction<string>>
+) => {
+  const fetchProductsFromContentLake =
+    async (): Promise<FetchProductsFromSanityContentLakeType> => {
+      try {
+        setIsFetching(true);
+        const productsData = await client.fetch(fetchAllProducts);
+        setIsFetching(false);
+        setContentLakeProducts(productsData as ContentLakeProductsType);
+        return productsData as FetchProductsFromSanityContentLakeType;
+      } catch (err) {
+        console.log("Error");
+        setErrorMessage(
+          "Error while fetching products, try reloading the page"
+        );
+        setIsFetching(false);
+        throw new Error("Error while fetching products");
+      }
+    };
   return {
     fetchProductsFromContentLake,
   };
---
+++ eco-site/src/types/contentLake.ts
@@ -0,0 +1,9 @@
+interface ContentLakeProductsType {
+  productDescription: string;
+  productPrice: number;
+  productTitle: string;
+  slug: string;
+}
+[];
+
+export { type ContentLakeProductsType };
