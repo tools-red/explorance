9fdb6ccd605db87b06df194f87ea5aacbe940c1b
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-16 03:40:00 +0530

    Defined: getServerSide Fetch function

--- eco-site/src/pages/products/[slug].tsx
+++ eco-site/src/pages/products/[slug].tsx
@@ -1,10 +1,25 @@
 import { Flex, Text } from "@chakra-ui/react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
+import useSanityContentLake from "~/hooks/useSanityContentLake";
 import BasePageLayout from "~/layouts/BasePageLayout";
+import { ContentLakeProductsType } from "~/types/contentLake";
 
-const ProductViewPage = () => {
+export const getServerSideProps = (async (context) => {
+  const { fetchProductDetailsFromContentLake } = useSanityContentLake();
+  const productData = await fetchProductDetailsFromContentLake(
+    context.query.slug as string
+  );
+  return { props: { productData } };
+}) satisfies GetServerSideProps<{
+  productData: ContentLakeProductsType | undefined;
+}>;
+
+const ProductViewPage = ({
+  productData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
-  console.log(router.query.slug);
+  console.log({ productData });
   return (
     <BasePageLayout>
       <Text>Hi this page is for {router.query.slug}</Text>
