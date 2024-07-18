fdb85437274effba5f12cfcd775231bbb7143817
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-19 05:19:42 +0530

    Created: Mail function for confirming purchase

--- eco-site/src/server/mail.ts
+++ eco-site/src/server/mail.ts
@@ -32,3 +32,15 @@ export const sendMail = async ({
     console.error("Error sending mail: ", e);
   }
 };
+
+const sendPurchaseMail = async (to: string, purchase_id: string) => {
+  const subject = `Indibliss Products Purchase Confirmed - ${purchase_id}`;
+  const html = `<div>
+  <p>Thank you for shopping at Indibliss, we hope to see you again too !!</p>
+  <p>Your Purchase ID is ${purchase_id}</p>
+  </div>`;
+
+  await sendMail({ to, subject, html });
+};
+
+export { sendPurchaseMail };
