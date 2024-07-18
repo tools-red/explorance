7e21c6856dedb690bf7b475fc0e201bd255e7791
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-18 18:17:41 +0530

    Created: TRPC Route for generating purchases

--- eco-site/src/server/api/routers/purchase.ts
+++ eco-site/src/server/api/routers/purchase.ts
@@ -1,5 +1,50 @@
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import * as Yup from "yup";
 
-const purchase = createTRPCRouter({});
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { CheckoutFormSchema } from "~/lib/formik/schemas";
+import { db } from "~/server/db";
+import { generatePurchaseId } from "~/utils/helpers";
+
+const purchase = createTRPCRouter({
+  createPurchase: publicProcedure
+    .input(Yup.object(CheckoutFormSchema.fields))
+    .mutation(async ({ input }) => {
+      try {
+        const {
+          address,
+          city,
+          email,
+          firstname,
+          lastname,
+          mobile,
+          postalcode,
+          state,
+        } = input;
+
+        const purchase_Id = generatePurchaseId();
+
+        const purchase_order = await db.purchases.create({
+          data: {
+            address: address as string,
+            city: city as string,
+            email: email as string,
+            mobile: mobile as string,
+            name: `${firstname} ${lastname}`,
+            postalcode: postalcode as string,
+            state: state as string,
+            purchase_id: purchase_Id,
+          },
+        });
+
+        return {
+          purchase_order: purchase_order,
+          orderState: true,
+        };
+      } catch (err) {
+        console.log(err);
+        throw new Error(`Couldn't process order: ${err}`);
+      }
+    }),
+});
 
 export default purchase;
