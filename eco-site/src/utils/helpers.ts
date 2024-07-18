f64ecdcd656bb641003f0a8f0f4766ad756223f6
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-18 18:03:18 +0530

    Added: Util to generate purchase ID

--- eco-site/src/utils/helpers.ts
+++ eco-site/src/utils/helpers.ts
@@ -3,4 +3,10 @@ const toTitleCase = (str: string) => {
   return result.charAt(0).toUpperCase() + result.slice(1);
 };
 
+const generatePurchaseId = () => {
+  const timestamp = Date.now().toString(36);
+  const randomChars = Math.random().toString(36).substring(2, 7);
+  return `PUR-${timestamp}${randomChars}`;
+};
+
 export { toTitleCase };
