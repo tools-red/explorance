2fddb9d548b58ee6468601477ca5fe9a9212ea8a
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-19 06:16:00 +0530

    Defined: Basic UI for admin page

--- eco-site/src/pages/admin/index.tsx
+++ eco-site/src/pages/admin/index.tsx
@@ -1,7 +1,28 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Input, Text } from "@chakra-ui/react";
+
+import Image, { StaticImageData } from "next/image";
+import BrandLogo from "../../../public/Indibliss_Logo.svg";
 
 const AdminPage = () => {
-  return <Flex>Admin Page here</Flex>;
+  return (
+    <Box py={10} maxW={1150} marginX={"auto"}>
+      <Flex gap={10} flexDir="column">
+        <Image
+          src={BrandLogo as StaticImageData}
+          width={150}
+          height={150}
+          alt="Brand Logo"
+        />
+        <Flex flexDir="column">
+          <Text className="marcellus-regular" fontSize="3xl">
+            Order Details
+          </Text>
+          <Text color="#797979">View your orders and manage transactions</Text>
+        </Flex>
+        <Input w={400} placeholder="Search for Orders" />
+      </Flex>
+    </Box>
+  );
 };
 
 export default AdminPage;
