daa18dae0919dd049c9555938b618032096a7d7b
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-20 18:30:23 +0530

    Defined: Orders Table UO

--- src/components/admin/Orders/index.tsx
+++ src/components/admin/Orders/index.tsx
@@ -8,31 +8,53 @@ import {
   Tr,
 } from "@chakra-ui/react";
 
-const Orders = () => {
+const OrderTableHeader: string[] = [
+  "Orders ID",
+  "Status",
+  "Order Amount",
+  "Customer name",
+  "Phone number",
+  "Email",
+];
+
+interface OrdersTableProps {}
+
+const Orders: React.FC<OrdersTableProps> = ({}) => {
   return (
     <Flex
       border="1px solid"
       borderRadius={20}
       borderColor="#E4E7EC"
-      p={5}
+      pt={5}
       gap={5}
       flexDir="column"
     >
-      <Text fontWeight={600} fontSize="large">
+      <Text px={5} fontWeight={600} fontSize="large">
         All Orders
       </Text>
       <TableContainer>
         <Table width="100%" minWidth="unset">
-          <Thead bg="gray.100">
+          <Thead>
             <Tr>
-              <Th>Order ID</Th>
-              <Th>Status</Th>
-              <Th>Order Date</Th>
-              <Th>Order Amount</Th>
-              <Th>Address</Th>
-              <Th>Customer Name</Th>
-              <Th>Phone Number</Th>
-              <Th>Email</Th>
+              {OrderTableHeader.map((header, index) => {
+                return (
+                  <Th
+                    borderTop="1px solid"
+                    borderBottom="1px solid"
+                    borderColor="#E4E7EC"
+                    bg="#F9FAFB"
+                    key={index}
+                  >
+                    <Text
+                      color="#475467"
+                      fontWeight={500}
+                      textTransform="capitalize"
+                    >
+                      {header}
+                    </Text>
+                  </Th>
+                );
+              })}
             </Tr>
           </Thead>
         </Table>
