acf84b7591e7b9d69f0b0c0bd33878ff32e92188
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-21 04:01:31 +0530

    Created: Order Details Modal

---
+++ src/components/admin/Orders/OrderDetailsModal.tsx
@@ -0,0 +1,38 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+
+interface OrderDetailsModalProps {
+  modalState: boolean;
+  handleModal: () => void;
+}
+
+const OrderDetailsModal: React.FC<OrderDetailsModalProps> = ({
+  handleModal,
+  modalState,
+}) => {
+  return (
+    <Modal isOpen={modalState} onClose={handleModal}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>Hi</ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={handleModal}>
+            Cancel
+          </Button>
+          <Button variant="ghost">Dispatch Delivery</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
