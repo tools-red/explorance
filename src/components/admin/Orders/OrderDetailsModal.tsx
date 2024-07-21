2418e31c57de745356bd4112c901f2ca88e43f60
akshatsabavat <akshat.sabavat@gmail.com>
2024-07-21 14:38:53 +0530

    Defined: Order details modal

--- src/components/admin/Orders/OrderDetailsModal.tsx
+++ src/components/admin/Orders/OrderDetailsModal.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,8 +8,11 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Text,
 } from "@chakra-ui/react";
 import { type FetchPurchasePromiseType } from "~/types/promises";
+
+import Image from "next/legacy/image";
 
 interface OrderDetailsModalProps {
   orderInView: FetchPurchasePromiseType | undefined;
@@ -22,23 +26,77 @@ const OrderDetailsModal: React.FC<OrderDetailsModalProps> = ({
   orderInView,
 }) => {
   return (
-    <Modal isOpen={modalState} onClose={handleModal}>
+    <Modal size="2xl" isOpen={modalState} onClose={handleModal}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>{`Order Details - ${orderInView?.purchase_id}`}</ModalHeader>
+        <ModalHeader>{`Order Details : ${orderInView?.purchase_id}`}</ModalHeader>
         <ModalCloseButton />
         {orderInView ? (
-          <ModalBody>Hi</ModalBody>
+          <ModalBody>
+            <Flex gap={10} flexDir="column">
+              <Flex gap={2} flexDir="column">
+                <Text fontWeight={600} fontSize="xl">
+                  {orderInView.name}
+                </Text>
+                <Flex gap={1} flexDir="column" fontSize="small">
+                  <Text color="gray.500">{`${orderInView.mobile}, ${orderInView.email}`}</Text>
+                  <Text color="gray.500">{orderInView.address}</Text>
+                  <Text color="gray.500">{`${orderInView.city}, ${orderInView.state}`}</Text>
+                  <Text color="gray.500">{orderInView.postalcode}</Text>
+                </Flex>
+              </Flex>
+
+              <Flex flexDir="column">
+                <Text fontSize="large" fontWeight={600}>
+                  Purchase Items
+                </Text>
+                <Flex
+                  height={200}
+                  overflowY="auto"
+                  my={5}
+                  gap={5}
+                  flexDir="column"
+                >
+                  {orderInView.purchase_details.items.map((item, index) => {
+                    return (
+                      <Flex key={index}>
+                        <Flex gap={3}>
+                          <Image
+                            src={item.productDetails.ImageURL}
+                            alt={`Image-Product-${item.productDetails.productId}`}
+                            width={90}
+                            height={130}
+                          />
+                          <Flex flexDir="column">
+                            <Text>{item.productDetails.productTitle}</Text>
+                            <Text>x {item.cartItemAmount}</Text>
+                          </Flex>
+                        </Flex>
+                      </Flex>
+                    );
+                  })}
+                </Flex>
+              </Flex>
+            </Flex>
+          </ModalBody>
         ) : (
           <ModalBody>Data not present</ModalBody>
         )}
 
         {orderInView ? (
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={handleModal}>
+            <Button
+              _hover={{ bg: "#F8E5DB" }}
+              bg="#FAF1EC"
+              color="#CC723F"
+              mr={3}
+              onClick={handleModal}
+            >
               Cancel
             </Button>
-            <Button variant="ghost">Dispatch Delivery</Button>
+            <Button _hover={{ bg: "#B45722" }} bg="#CC723F" color="white">
+              Dispatch Delivery
+            </Button>
           </ModalFooter>
         ) : (
           <></>
